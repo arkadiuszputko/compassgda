413c4dc1ff5e8e2465bed2114fc9e5228ce35f0d
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2013-09-19 23:28:30 +0200

    added photo service, venue view and stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('compassgdaApp', ['ngRoute', 'foursquareAPI', 'venues'])
+angular.module('compassgdaApp', ['ngRoute', 'foursquareAPI', 'photo', 'venues'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/venue/:venueId', {
+        templateUrl: 'views/venue.html',
+        controller: 'VenueCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,17 @@
 
 angular.module('compassgdaApp')
     .controller('MainCtrl', function ($scope, foursquareService, venuesService) {
-        var date = moment().format('YYYYMMDD');
+        var date = moment().format('YYYYMMDD'),
+            offset = 0;
+
 
         foursquareService.query({
                 ll: '54.3520252,18.6466384',
                 v: date,
-                locale: 'en'
+                locale: 'en',
+                limit: 10,
+                section: 'food',
+                offset: offset
             },
             function success (res) {
                 $scope.venues = venuesService.decorateVenues(res.response.groups[0].items);
